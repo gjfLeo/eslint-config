86503b135e1bf0f690b0f5af9ebb12a4c05744f3
gjfLeo <gjfLeo@yeah.net>
2023-05-30 09:41:31 +0800

    update rules

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -129,7 +129,7 @@ module.exports = {
     "semi": "off",
     "@typescript-eslint/semi": ["error", "always"],
     "quotes": "off",
-    "@typescript-eslint/quotes": ["error", "double"],
+    "@typescript-eslint/quotes": ["error", "double", { avoidEscape: true }],
     "space-before-blocks": "off",
     "@typescript-eslint/space-before-blocks": ["error", "always"],
     "space-before-function-paren": "off",
