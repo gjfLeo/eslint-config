f7c75d3888dc4a11e853be5a4ce9d021f80afe76
Liger <gjfleo@yeah.net>
2023-06-27 21:22:38 +0800

    update rules

--- packages/eslint-plugin-gjfleo/src/index.ts
+++ packages/eslint-plugin-gjfleo/src/index.ts
@@ -5,6 +5,7 @@ import preferInlineTypeImport from "./rules/prefer-inline-type-import";
 import topLevelFunction from "./rules/top-level-function";
 import noTsExportEqual from "./rules/no-ts-export-equal";
 import noCjsExports from "./rules/no-cjs-exports";
+import customSortKeys from "./rules/custom-sort-keys";
 
 export default {
   rules: {
@@ -15,5 +16,6 @@ export default {
     "top-level-function": topLevelFunction,
     "no-cjs-exports": noCjsExports,
     "no-ts-export-equal": noTsExportEqual,
+    "custom-sort-keys": customSortKeys,
   },
 };
