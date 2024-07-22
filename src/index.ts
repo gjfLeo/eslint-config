eccd556c12d8d3b136f45b5588ae26ae59918b96
Liger <gjfleo@yeah.net>
2024-07-22 21:41:17 +0800

    feat: enable formatter

--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,11 @@ export default function gjfleo(...userConfigs: Parameters<typeof antfu>[1][]): R
 
       unocss: isPackageExists("@unocss/eslint-plugin"),
 
-      // formatters: {
-      //   css: true,
-      // },
+      formatters: {
+        css: true,
+        html: true,
+        markdown: true,
+      },
     },
 
     {
